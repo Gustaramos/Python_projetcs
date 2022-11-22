c4537f2e37158eb942b7f59d4601df1bcba4989a
Cataflam <guram.sil@outlook.com>
2022-11-22 14:57:26 -0300

    Commit Att

--- AnaliseBonusViagem/BuscaVendedor.py
+++ AnaliseBonusViagem/BuscaVendedor.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from twilio.rest import Client
 
-account_sid = 'AXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'XXXXXXXXXXXXXXXXXXXXXX'
+account_sid = 'ACd63eb163d3971f617878714006272fa2'
+auth_token = 'b36ab1a8d06142842fdb13e0c742163d'
 
 client = Client(account_sid, auth_token)
-
 
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
@@ -15,7 +14,7 @@ for mes in lista_meses:
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
         message = client.messages.create(
-            to="+1111222222222",
-            from_="+1122333333333",
+            to="+5543998065774",
+            from_="+15625737048",
             body=f'No mês de {mes} alguém conseguiu bater a meta!\nVendedor: {vendedor}, Vendas{vendas}')
         print(message.sid)
