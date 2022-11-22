37fa443d278b232483616472c453129a68c195d3
Cataflam <guram.sil@outlook.com>
2022-11-22 15:12:21 -0300

    Commit Att

--- AnaliseBonusViagem/BuscaVendedor.py
+++ AnaliseBonusViagem/BuscaVendedor.py
@@ -14,7 +14,7 @@ for mes in lista_meses:
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
         message = client.messages.create(
-            to="+5543998065774",
-            from_="+15625737048",
+            to="+11111111111111111",
+            from_="+1111111111",
             body=f'No mês de {mes} alguém conseguiu bater a meta!\nVendedor: {vendedor}, Vendas{vendas}')
         print(message.sid)
