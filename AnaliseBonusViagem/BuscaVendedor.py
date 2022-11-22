d270321a7f037ba92bb4affd147d92b301eb32c6
Cataflam <guram.sil@outlook.com>
2022-11-22 15:13:29 -0300

    Update to BuscaVendedor

--- AnaliseBonusViagem/BuscaVendedor.py
+++ AnaliseBonusViagem/BuscaVendedor.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from twilio.rest import Client
 
-account_sid = 'ACd63eb163d3971f617878714006272fa2'
-auth_token = 'b36ab1a8d06142842fdb13e0c742163d'
+account_sid = 'AXXXXXXXXXXXXXXXXXXX'
+auth_token = 'XXXXXXXXXXXXXXXXXXXXXX'
 
 client = Client(account_sid, auth_token)
 
@@ -14,7 +14,7 @@ for mes in lista_meses:
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
         message = client.messages.create(
-            to="+11111111111111111",
-            from_="+1111111111",
+            to="+1111111111111",
+            from_="+1111111111111",
             body=f'No mês de {mes} alguém conseguiu bater a meta!\nVendedor: {vendedor}, Vendas{vendas}')
         print(message.sid)
