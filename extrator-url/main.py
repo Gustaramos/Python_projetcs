cdc6904356aea55d8c94ad3e07841aeffd631c19
Cataflam <guram.sil@outlook.com>
2022-11-29 17:56:37 -0300

    Commit Att

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,10 +1,16 @@
-url = 'https://bytbank.com/cambio?moedaOrigem=real'
-print(url.find('?'))
-print(url)
+url = 'https://bytbank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
 
 indice_interrogacao = url.find('?')
 url_base = url[0:indice_interrogacao]
-print(url_base)
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+parametro_de_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_de_busca)
+
+print(indice_parametro)
+
+indice_valor = indice_parametro + len(parametro_de_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
