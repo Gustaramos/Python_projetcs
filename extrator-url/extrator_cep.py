929f7beabacbbc32d27e3da60db30174d7ef83d9
Cataflam <guram.sil@outlook.com>
2022-12-08 14:40:12 -0300

    Commit att

--- extrator-url/extrator_cep.py
+++ extrator-url/extrator_cep.py
@@ -6,7 +6,7 @@ import re    #RegEx --- Expressões regulares
 # 5 digitos + hífen (opcional) + 3 digitos
 
 # Criando padrão com método compile. Após definir o padrão, atribuir a uma variavel.
-padrao = re.compile('[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]')
+padrao = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
 # Após definir o padrão, fazer a busca dele dentro de uma string
 busca = padrao.search(endereco)    #Match
 if busca:
--- extrator-url/extrator_url.py
+++ extrator-url/extrator_url.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitizacao_url(url)
@@ -12,8 +14,11 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:
             raise ValueError('A URL esta vazia')
-        if not self.url.startswith('https') or not self.get_url_base().endswith('/cambio'):
-            raise ValueError('A URL não é válida')
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError('A URL não é valida')
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -29,17 +34,19 @@ class ExtratorURL:
         indice_parametro = self.get_url_parametros().find(parametro_de_busca)
         indice_do_valor = indice_parametro + len(parametro_de_busca) + 1
         indice_e_comercial = self.get_url_parametros().find('&', indice_do_valor)
+
         if indice_e_comercial == -1:
             valor = self.get_url_parametros[indice_do_valor:]
         else:
             valor = self.get_url_parametros()[indice_do_valor:indice_e_comercial]
         return valor
 
-#extrator_url = ExtratorURL(None)
-extrator_url = ExtratorURL('https://bytbank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real')
-
-#extrator_url = ExtratorURL('//bytbank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real')
-#extrator_url = ExtratorURL('https://bytbank.com?moedaDestino=dolar&quantidade=100&moedaOrigem=real')
-
+url = 'https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar0&moedaOrigem=real'
+extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)
+
+
+#extrator_url = ExtratorURL(None)
+#extrator_url = ExtratorURL('//bytbank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real')
+#extrator_url = ExtratorURL('https://bytbank.com?moedaDestino=dolar&quantidade=100&moedaOrigem=real')
